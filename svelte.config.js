f158bb86b6b134df7536fec732dd989b7b7362c1
jackbisceglia <jackbisceglia2000@gmail.com>
2023-02-03 13:46:13 -0500

    revert

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,5 @@
-import adapter_auto from '@sveltejs/adapter-auto';
-import adapter_netlify from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const getAdapterByEnv = () => {
-	if (process.env.NETLIFY) {
-		return adapter_netlify({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		});
-	} else {
-		return adapter_auto();
-	}
-};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -26,7 +8,7 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: getAdapterByEnv()
+		adapter: adapter()
 	}
 };
 
