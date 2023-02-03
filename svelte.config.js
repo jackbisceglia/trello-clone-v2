4c8f3c6111bab16f7d790fae325b455619406cfe
jackbisceglia <jackbisceglia2000@gmail.com>
2023-02-03 13:37:20 -0500

    fix adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,9 @@
 import adapter_auto from '@sveltejs/adapter-auto';
-import adapter_vercel from '@sveltejs/adapter-vercel';
 import adapter_netlify from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const getAdapterByEnv = () => {
-	if (process.env.VERCEL) {
-		return adapter_vercel;
-	} else if (process.env.NETLIFY) {
+	if (process.env.NETLIFY) {
 		return adapter_netlify;
 	} else {
 		return adapter_auto;
